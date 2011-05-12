10666edbea959ff9e5d02bb1cb09b3c355ef5ca2
cmarcusa <cmarcusa@hexacta.com>
2011-05-12 09:57:46 -0300

    Viendo si puedo agregar el src

---
+++ tacs-tp/src/main/java/org/tacs/tp/servlet/RESTConsumer.java
@@ -0,0 +1,22 @@
+//package org.tacs.tp.servlet;
+//
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//
+//import javax.servlet.Servlet;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//public class RESTConsumer extends HttpServlet implements Servlet {
+//
+//	private static final long serialVersionUID = 1L;
+//
+//	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+//		PrintWriter out = res.getWriter();
+//
+//		out.println("Hello, Men!");
+//		out.close();
+//	}
+//}
