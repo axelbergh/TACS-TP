19f7bddf3a0b343defd50de293bb01eed3eef381
korva <mcorbaninicasey@gmail.com>
2011-06-17 01:16:25 -0300

    servlet RESTcomsumer by chaito andando, para seguir con el tema de persistencia

--- tacs-tp/src/main/java/org/tacs/tp/servlet/RESTConsumer.java
+++ tacs-tp/src/main/java/org/tacs/tp/servlet/RESTConsumer.java
@@ -1,22 +1,21 @@
-//package org.tacs.tp.servlet;
-//
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//
-//import javax.servlet.Servlet;
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//public class RESTConsumer extends HttpServlet implements Servlet {
-//
-//	private static final long serialVersionUID = 1L;
-//
-//	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-//		PrintWriter out = res.getWriter();
-//
-//		out.println("Hello, Men!");
+package org.tacs.tp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RESTConsumer extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+
+		out.println("Hello, Men!");
 //		out.close();
-//	}
-//}
+	}
+}
