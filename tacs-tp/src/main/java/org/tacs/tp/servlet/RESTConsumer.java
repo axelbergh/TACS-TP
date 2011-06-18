6964ac2a36d9f00c716633c49176e792dafe977a
korva <mcorbaninicasey@gmail.com>
2011-06-17 22:02:06 -0300

    avances con el servlet

--- tacs-tp/src/main/java/org/tacs/tp/servlet/RESTConsumer.java
+++ tacs-tp/src/main/java/org/tacs/tp/servlet/RESTConsumer.java
@@ -1,21 +1,36 @@
 package org.tacs.tp.servlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 public class RESTConsumer extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-
-		out.println("Hello, Men!");
-//		out.close();
+			    response.setContentType("text/html");
+			    PrintWriter out = response.getWriter();
+			    out.println("<BODY>\n" +
+			                "<H1 ALIGN=CENTER></H1>\n" +
+			                "<UL>\n" +
+			                "  <LI>param1: "
+			                + request.getParameter("param1") + "\n" +
+			                "  <LI>param2: "
+			                + request.getParameter("param2") + "\n" +
+			                "  <LI>param3: "
+			                + request.getParameter("param3") + "\n" +
+			                "</UL>\n" +
+			                "</BODY></HTML>");
+			    out.close();
 	}
+
+	  public void doPost(HttpServletRequest request,
+	                     HttpServletResponse response)
+	      throws ServletException, IOException {
+	    doGet(request, response);
+	  }
+		
 }
+
