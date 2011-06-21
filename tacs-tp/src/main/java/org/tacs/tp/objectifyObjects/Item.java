f5fba2754829510da7073b890800ac001016d851
Diego Berkovics <dieguitob@gmail.com>
2011-06-20 21:31:33 -0300

    Agregar a favoritos

---
+++ tacs-tp/src/main/java/org/tacs/tp/objectifyObjects/Item.java
@@ -0,0 +1,35 @@
+package org.tacs.tp.objectifyObjects;
+
+import javax.persistence.*;
+
+public class Item {
+	
+	@Id
+	private String id;
+	private String nombre;
+	private String fotoURL;
+	private String linkURL;
+	
+	@SuppressWarnings("unused")
+	private Item(){};
+	
+	public Item(String id, String nombre, String fotoURL, String linkURL){
+		this.id = id;
+		this.nombre = nombre;
+		this.fotoURL = fotoURL;
+		this.linkURL = linkURL;
+	}
+	
+	public String getNombre(){
+		return this.nombre;
+	}
+	public String getId(){
+		return this.id;
+	}
+	public String getFotoURL(){
+		return this.fotoURL;
+	}
+	public String getLinkURL(){
+		return this.linkURL;
+	}
+}
